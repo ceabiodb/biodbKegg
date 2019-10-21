9ee0b6073c51ff7f4bbfc1f8862c29c00cb75064
Pierrick Roger <pk.roger@icloud.com>
2019-10-21 10:57:17 +0200

    Import KEGG files from biodb.

---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+# vi: fdm=marker
+# Script needed to run testthat automatically from ‘R CMD check’. See
+# testthat::test_dir documentation.
+library(testthat)
+library(biodbKegg)
+Sys.setenv(TESTTHAT_REPORTER = "summary")
+test_check("biodbKegg")
