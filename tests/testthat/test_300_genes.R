5059dde489a5e9e5a2a0728bb6913166d9f52d76
Pierrick Roger <pk.roger@icloud.com>
2020-07-29 08:50:04 +0200

    Add getPathwayIds() functions to KEGG Genes.

--- tests/testthat/test_300_genes.R
+++ tests/testthat/test_300_genes.R
@@ -1,3 +1,21 @@
+test.kegg.genes.getPathwayIdsPerGene <- function(conn) {
+    id <- 'mmu:14635'
+	ids <- conn$getPathwayIdsPerGene(id, org='mmu')
+	testthat::expect_is(ids, 'list')
+	testthat::expect_true(id %in% names(ids))
+	testthat::expect_is(ids[[id]], 'character')
+	testthat::expect_true(length(ids[[id]]) > 0)
+    print(ids)
+}
+
+test.kegg.genes.getPathwayIds <- function(conn) {
+    id <- 'mmu:14635'
+    ids <- conn$getPathwayIds(id, org='mmu')
+    testthat::expect_is(ids, 'character')
+    testthat::expect_true(length(ids) > 0)
+    print(ids)
+}
+
 # Main
 ################################################################
 
@@ -16,6 +34,10 @@ conn <- biodb$getFactory()$createConn('kegg.genes')
 
 # Run tests
 biodb::runGenericTests(conn)
+biodb::testThat('getPathwayIdsPerGene() works correctly.',
+                test.kegg.genes.getPathwayIdsPerGene, conn=conn)
+biodb::testThat('getPathwayIds() works correctly.',
+                test.kegg.genes.getPathwayIds, conn=conn)
 
 # Terminate Biodb
 biodb$terminate()
